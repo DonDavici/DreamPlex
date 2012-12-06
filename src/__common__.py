90ee061e969e601cf713524513b806accdb52a6b
DonDavici <dondavici@gmail.com>
2012-12-06 21:19:38 +0100

    small tweaks

--- src/__common__.py
+++ src/__common__.py
@@ -38,17 +38,6 @@ from DPH_Singleton import Singleton
 gConnectivity = None
 gLogFile = None
 
-#########################################
-#Black       0;30     Dark Gray     1;30#
-#Blue        0;34     Light Blue    1;34#
-#Green       0;32     Light Green   1;32#
-#Cyan        0;36     Light Cyan    1;36#
-#Red         0;31     Light Red     1;31#
-#Purple      0;35     Light Purple  1;35#
-#Brown       0;33     Yellow        1;33#
-#Light Gray  0;37     White         1;37#
-#########################################
-
 # ****************************** VERBOSITY Level *******************************
 VERB_ERROR       = 0  # "E" shows error
 VERB_INFORMATION = 0  # "I" shows important highlights to have better overview if something really happening or not
@@ -70,7 +59,6 @@ CLOSING_MESSAGE = "LEAVING"
 #===============================================================================
 def printl2 (string, parent=None, dmode= "U"):
 	'''
-	add colors to screen output and sends output to logfile due to verbose level
 	ConfigSelection(default="0", choices = [("0", _("Silent")),("1", _("Normal")),("2", _("High")),("3", _("All"))])
 	
 	@param string: 
@@ -352,6 +340,7 @@ def checkPlexEnvironment():
 	checkDirectory(mediaFolder)
 	
 	printl2("","__common__::checkPlexEnvironment", "C")
+	
 #===============================================================================
 # 
 #===============================================================================
