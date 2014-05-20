9aa8650fb13acf5fda936aa43a80ac076718f107
DonDavici <dondavici@gmail.com>
2014-05-20 22:37:05 +0200

    avoid greenscreen in menucallback

--- src/DP_View.py
+++ src/DP_View.py
@@ -1450,10 +1450,12 @@ class DP_View(Screen, NumericalTextInput):
 	#===========================================================================
 	def displayOptionsMenuCallback(self, choice):
 		printl("", self, "S")
-		printl("choice: " + str(choice[1]), self, "D")
 
 		if choice is None or choice[1] is None:
+			printl("choice: None - we pressed exit", self, "D")
 			return
+
+		printl("choice: " + str(choice[1]), self, "D")
 
 		if choice[1].fnc:
 			printl("5", self, "D")
