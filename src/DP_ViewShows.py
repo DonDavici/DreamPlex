d03fa8e377642989f1c63cb8e52171daca20c4f8
DonDavici <dondavici@gmail.com>
2014-08-03 11:04:36 +0200

    avoid gs in studio text

--- src/DP_ViewShows.py
+++ src/DP_ViewShows.py
@@ -83,7 +83,7 @@ class DPS_ViewShows(DP_View):
 			self["viewedLeafCount"].setText(self.details.get("viewedLeafCount", " "))
 			self["unviewedLeafCount"].setText(str(int(self.details.get("leafCount", " ")) - int(self.details.get("viewedLeafCount", " "))))
 			self["childCount"].setText(str(self.details.get("childCount", " ")))
-			self["studio"].setText(self.details.get("studio", " "))
+			self["studio"].setText(encodeMe(self.details.get("studio", " ")))
 			self["genre"].setText(self.details.get("genre", " "))
 			self["year"].setText(str(self.details.get("year", " - ")))
 
