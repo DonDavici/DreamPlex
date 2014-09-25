9e58d22f03d47edd38e2800c44a6a74f6d50a055
DonDavici <dondavici@gmail.com>
2014-09-25 08:55:35 +0200

    remoteplayer binds now to all interfaces with 0.0.0.0

--- src/DPH_RemoteListener.py
+++ src/DPH_RemoteListener.py
@@ -140,7 +140,7 @@ class HttpDeamon(Thread):
 	#def runHttp(session, playerCallback, HandlerClass = MyHandler,ServerClass = HTTPServer, protocol="HTTP/1.0"):
 	def run(self):
 		printl("", __name__, "S")
-		server_address = (self.myIp, self.port)
+		server_address = ("", self.port)#(self.myIp, self.port)
 
 		self.HandlerClass.protocol_version = self.protocol
 		self.HandlerClass.session = self.session
